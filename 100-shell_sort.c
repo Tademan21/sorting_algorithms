dc52206340d3f46f6feaf14e8f61cf36fc8dc735
Tademan21 <101402625+Tademan21@users.noreply.github.com>
2022-08-15 10:34:09 +0300

    Update 100-shell_sort.c

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,34 +1,35 @@
-nclude "sort.h"
+#include "sort.h"
 
 /**
-* shell_sort - Sorts array using Knuth Sequence of Shell sort
-* @array: Array to be sorted
-* @size: Size of array to sort
+* shell_sort - function sorts an array of integers using
+* the shell sort algorithm with Knuth sequence
+* @array: the array of integers
+* @size: size of the array
+*
+* Return: none
 */
-
 void shell_sort(int *array, size_t size)
 {
-	size_t i, gap = 1, k;
-	int v;
+	size_t gap = 0, j, i;
+	int tmp;
 
-	if (array == NULL)
+	if (size < 2)
 		return;
-	while (gap < size)
-		gap = 3 * gap + 1;
-	if (gap >= size)
-		gap = (gap - 1) / 3;
-	while (gap > 0)
+
+	while ((gap = gap * 3 + 1) < size)
+		;
+
+	gap = (gap - 1) / 3;
+
+	for (; gap > 0; gap = (gap - 1) / 3)
 	{
 		for (i = gap; i < size; i++)
 		{
-			v = array[i];
-			for (k = i; (k > gap - 1) && (v < array[k - gap]); k -= gap)
-			{
-				array[k] = array[k - gap];
-			}
-			array[k] = v;
+			tmp = array[i];
+			for (j = i; j >= gap && tmp <= array[j - gap]; j -= gap)
+				array[j] = array[j - gap];
+			array[j] = tmp;
 		}
-		gap = (gap - 1) / 3;
 		print_array(array, size);
 	}
 }
